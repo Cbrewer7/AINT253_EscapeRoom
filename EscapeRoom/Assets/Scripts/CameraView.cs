7bb617f6d699a9a55133e3e87920e2521272d0e8
BRE15150326 <C-brewer@hotmail.co.uk>
2020-01-04 20:04:40 +0000

    All models + scripts
    All models have been added to the scene fully textured, and playermovement along with player pickup scripts have been added.

---
+++ EscapeRoom/Assets/Scripts/CameraView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour
+{
+
+    //Credit to Brackeys for camera view & Movement
+    // https://www.youtube.com/watch?v=_QajrabyTJc
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        //Locks the cursor at the centre of the screen (Cursor is not visible)
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Prevents looking past over and below the player
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ EscapeRoom/Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+    //Original Credit to Jimmy Vegas for pickup tutorial
+    // https://www.youtube.com/watch?v=IEV64CLZra8&list=WL&index=6&t=0s
+
+    public Transform destination;
+
+    void OnMouseDown()
+    {
+        if(GetComponent<BoxCollider>())
+        {
+            GetComponent<BoxCollider>().enabled = false;
+        }
+
+        if (GetComponent<MeshCollider>())
+        {
+            GetComponent<MeshCollider>().enabled = false;
+        }
+
+        GetComponent<Rigidbody>().useGravity = false;
+
+        //Move position of object infront to the destination object
+        this.transform.position = destination.position;
+
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+
+    //When mouse is let go object returns to normal
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+
+        if (GetComponent<BoxCollider>())
+        {
+            GetComponent<BoxCollider>().enabled = true;
+        }
+
+        if (GetComponent<MeshCollider>())
+        {
+            GetComponent<MeshCollider>().enabled = true;
+        }
+    }
+}
